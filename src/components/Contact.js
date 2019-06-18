c133c29094155cbc9ee7f2dc8b537a120e28bcd8
abrao90 <anirudhrao2490@gmail.com>
2019-06-18 05:38:06 +0200

    second

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,18 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="#">
+                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
+                        <input type="hidden" name="bot-field" />
+                        <input type="hidden" name="form-name" value="contact" />
                     </div>
                     <div className="field half">
                         <label htmlFor="email">Email</label>
                         <input type="text" name="email" id="email" />
+                        <input type="hidden" name="bot-field" />
+                        <input type="hidden" name="form-name" value="contact" />
                     </div>
                     <div className="field">
                         <label htmlFor="message">Message</label>
@@ -19,6 +23,8 @@ const Contact = (props) => (
                     </div>
                     <ul className="actions">
                         <li><input type="submit" value="Send Message" className="special" /></li>
+                        <input type="hidden" name="bot-field" />
+                        <input type="hidden" name="form-name" value="contact" />
                         <li><input type="reset" value="Clear" /></li>
                     </ul>
                 </form>
@@ -28,14 +34,14 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="#">hello@gohugapet.com</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <span>030 233 277 42</span>
                     </div>
                 </section>
                 <section>
@@ -43,8 +49,8 @@ const Contact = (props) => (
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
                         <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
+                        Berlin, DE 00000<br />
+                        Germany</span>
                     </div>
                 </section>
             </section>
